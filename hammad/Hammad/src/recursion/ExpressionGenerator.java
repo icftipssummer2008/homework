023be9b786c435d973bfc3e194247e5549ad7e8f
Hammad Muhammad <hammad.muhammad@safeway.com>
2018-07-16 14:20:05 -0700

    Q6 - Expression Generator (recursion)

---
+++ hammad/Hammad/src/recursion/ExpressionGenerator.java
@@ -0,0 +1,41 @@
+package recursion;
+
+import java.util.HashSet;
+
+public class ExpressionGenerator {
+	
+	static String[] operators = {"", "+", "*"};
+	
+	public static void main(String[] args) {
+		generateExpression("222");
+	}
+	
+	public static void generateExpression(String str) {
+		HashSet<String> hashset = new HashSet<>();
+		generateExpressionHelper(str, 0, "",  hashset);
+		printSet(hashset);
+	}
+	
+	public static void printSet(HashSet<String> hashset) {
+		for (String str : hashset) {
+			System.out.println(str);
+		}
+	}
+	
+	public static void generateExpressionHelper(String input, int i, String output, HashSet<String> hashset) {
+		int inputLength = input.length();
+		if(i==inputLength) {
+			hashset.add(output);
+			return;
+		}
+		
+		for (String operator : operators) {
+			String temp = output + input.charAt(i);
+			if( i < inputLength -1) {
+				temp = temp + operator;
+			}
+			generateExpressionHelper(input, i+1, temp, hashset);
+
+		}		
+	}
+}
